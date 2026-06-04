1863b6e09e0b61d8de0d78db14ac8a3506a0d440
Dakshhhhh-ops <24bee040@nith.ac.in>
2026-06-05 00:26:12 +0530

    hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import dill
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.metrics import r2_score
 
@@ -19,12 +20,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train, y_train, X_test, y_test, models): 
+def evaluate_model(X_train, y_train, X_test, y_test, models, params): 
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]  # get model object
+            para = params.get(list(models.keys())[i], {})  # get parameters for the model
+
+            gs=GridSearchCV(model, para, cv=3)
+            gs.fit(X_train,y_train)  # create GridSearchCV object
+
+            model.set_params(**gs.best_params_)  # set best parameters to the model
             model.fit(X_train, y_train)  # fit model on train data
+
+            #model.fit(X_train, y_train)  # fit model on train data
+
             y_train_pred = model.predict(X_train)  # predict on train data
             y_test_pred = model.predict(X_test)  # predict on test data
             train_model_score = r2_score(y_train, y_train_pred)  # calculate r2 score for train data
